e5c2da80a82bd0f82a65b617332470ce4ce7dbc0
Baduk <iankurtis@yandex.ru>
2023-09-26 14:49:08 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 100; // начальный счёт
-        int y = 1500; // сумма пополения
+        int y = 400; // сумма пополения
         int z = y / 100; // бонусы
         boolean i = y > 1000; // условие
 
